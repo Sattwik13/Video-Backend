9b794e9fd7716ae2bcdd5d92bba20a74dc3c1681
Sattwik13 <sattwikmanna6359@gmail.com>
2024-04-13 19:53:33 +0530

    bug fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -20,7 +20,7 @@ router.route("/register").post(
     registerUser
 )
 
-router.route("\login").post(loginUser)
+router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
