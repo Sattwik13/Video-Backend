ce7b1afc8db4998c6927ea9942eaf22607ee20bf
Sattwik13 <sattwikmanna6359@gmail.com>
2024-04-14 22:45:50 +0530

    fixed refershToken validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -213,7 +213,7 @@ const refershAccessToken = asyncHandler(async (req, res) =>
     const incomingRefreshToken = req.cookies.refreshToken || 
     req.body.refreshToken
 
-    if(incomingRefreshToken)
+    if(!incomingRefreshToken)
      {
         throw new ApiError(401, "unauthorized request")
      }
