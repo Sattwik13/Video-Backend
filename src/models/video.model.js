649df00c256a19b801ed492b98e097bc781a0861
Sattwik13 <sattwikmanna6359@gmail.com>
2024-04-19 03:25:57 +0530

    Add comment, like, playlist and tweet models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema= new Schema (
     {
@@ -41,3 +41,7 @@ const videoSchema= new Schema (
         timestamps: true
     }
 )
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
